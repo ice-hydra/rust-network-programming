1b6674f1a39aaff85a51012e42dac60b697b5f59
ice-hydra <icehydra666@gmail.com>
2021-10-16 17:20:48 +0800

    udp server

--- udp-server/src/main.rs
+++ udp-server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:8080")?;
+    loop {
+        let mut buf = [0u8;512];
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        println!("[READ] size: {}, src: {:?}", amt, src);
+
+        let buf = &mut buf[..amt];
+        buf.reverse();
+        let sended_size =  socket.send_to(buf, src)?;
+        println!("[SEND] size: {}, src: {:?} ", sended_size, src);
+    }
 }
